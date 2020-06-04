1d8194b68df2956f60b79383c57e0f9c56126293
Hayao Gai <hayaogai@gmail.com>
2020-06-04 17:38:49 +0800

    Add files via upload
    Fixed a bug that it won't work if post make comment function disable.

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Download photo or video by one button click.
 
-![image](https://raw.githubusercontent.com/HayaoGai/Instagram-Download-Photo-Video/master/example.jpg)
+<img width="700" src="https://raw.githubusercontent.com/HayaoGai/Instagram-Download-Photo-Video/master/example.jpg">
+<br>
+<a href="https://www.buymeacoffee.com/hayaogai" target="_blank"><img width="200" src="https://cdn.buymeacoffee.com/buttons/arial-green.png" alt="Buy Me A Coffee"></a>
--- userscript.js
+++ userscript.js
@@ -1,22 +1,22 @@
 // ==UserScript==
-// @name               Instagram: Download Photo & Video
-// @name:zh-TW         Instagram 下載照片、影片
-// @name:zh-CN         Instagram 下载照片、视频
-// @name:ja            Instagram 写真とビデオのダウンロード
-// @name:ko            Instagram 사진 및 비디오 다운로드
-// @name:ru            Instagram скачать фото и видео
-// @version            1.0.12
-// @description        Download photo or video by one button click.
-// @description:zh-TW  即按下載 Instagram 照片或影片。
-// @description:zh-CN  一键下载 Instagram 照片或视频。
-// @description:ja     ボタンをクリックするだけで写真やビデオをダウンロードできます。
-// @description:ko     한 번의 클릭으로 사진 또는 비디오를 다운로드하십시오.
-// @description:ru     Скачать фото или видео одним нажатием кнопки.
-// @author             Hayao-Gai
-// @namespace          https://github.com/HayaoGai
-// @icon               https://i.imgur.com/obCmlr9.png
-// @match              https://www.instagram.com/*
-// @grant              none
+// @name                Instagram: Download Photo & Video
+// @name:zh-TW          Instagram 下載照片、影片
+// @name:zh-CN          Instagram 下载照片、视频
+// @name:ja             Instagram 写真とビデオのダウンロード
+// @name:ko             Instagram 사진 및 비디오 다운로드
+// @name:ru             Instagram скачать фото и видео
+// @version             1.0.14
+// @description         Download photo or video by one button click.
+// @description:zh-TW   即按下載 Instagram 照片或影片。
+// @description:zh-CN   一键下载 Instagram 照片或视频。
+// @description:ja      ボタンをクリックするだけで写真やビデオをダウンロードできます。
+// @description:ko      한 번의 클릭으로 사진 또는 비디오를 다운로드하십시오.
+// @description:ru      Скачать фото или видео одним нажатием кнопки.
+// @author              Hayao-Gai
+// @namespace           https://github.com/HayaoGai
+// @icon                https://i.imgur.com/obCmlr9.png
+// @match               https://www.instagram.com/*
+// @grant               none
 // ==/UserScript==
 
 /* jshint esversion: 6 */
@@ -28,52 +28,51 @@
     // iconNewtab made by https://www.flaticon.com/authors/those-icons
     const iconDownload = `<svg width="24" height="24" viewBox="0 0 512 512"><g><g><path d="M472,313v139c0,11.028-8.972,20-20,20H60c-11.028,0-20-8.972-20-20V313H0v139c0,33.084,26.916,60,60,60h392 c33.084,0,60-26.916,60-60V313H472z"></path></g></g><g><g><polygon points="352,235.716 276,311.716 276,0 236,0 236,311.716 160,235.716 131.716,264 256,388.284 380.284,264"></polygon></g></g></svg>`;
     const iconNewtab = `<svg width="24" height="24" viewBox="0 0 482.239 482.239"><path d="m465.016 0h-344.456c-9.52 0-17.223 7.703-17.223 17.223v86.114h-86.114c-9.52 0-17.223 7.703-17.223 17.223v344.456c0 9.52 7.703 17.223 17.223 17.223h344.456c9.52 0 17.223-7.703 17.223-17.223v-86.114h86.114c9.52 0 17.223-7.703 17.223-17.223v-344.456c0-9.52-7.703-17.223-17.223-17.223zm-120.56 447.793h-310.01v-310.01h310.011v310.01zm103.337-103.337h-68.891v-223.896c0-9.52-7.703-17.223-17.223-17.223h-223.896v-68.891h310.011v310.01z"></path></svg>`;
+    let currentUrl = document.location.href;
     let updating = false;
 
-    init();
+    init(10);
+
     locationChange();
+
     window.addEventListener("scroll", update);
 
-    function update() {
-        if (updating) return;
-        updating = true;
-        init();
-        setTimeout(() => { updating = false; }, 1000);
+    function init(times) {
+        for (let i = 0; i < times; i++) {
+            setTimeout(addButton, 500 * i);
+            setTimeout(checkSort, 500 * i);
+        }
     }
 
-    function init(retry = 0) {
-        // get
-        const panels = document.querySelectorAll("section.ltpMr.Slqrh:not(.section-set)");
-        // check
-        if (!panels.length && retry < 10) {
-            setTimeout(() => init(retry + 1), 500);
-            return;
-        }
-        // section
-        panels.forEach(panel => {
+    function addButton() {
+        // get panel
+        document.querySelectorAll("section.ltpMr.Slqrh:not(.section-set)").forEach(panel => {
             panel.classList.add("section-set");
-            waitInstagram(panel);
+            // button 1: download
+            // firefox doesn't support direct download function.
+            const isFirefox = typeof InstallTrigger !== 'undefined';
+            if (!isFirefox) setButton(panel, "download-set", iconDownload);
+            // button 2: new tab
+            setButton(panel, "newtab-set", iconNewtab);
         });
     }
 
-    function waitInstagram(panel, retry = 0) {
-        // wait until instagram ready.
-        if (panel.childNodes.length < 4 && retry < 10) {
-            setTimeout(() => waitInstagram(panel, retry + 1), 500);
-            return;
-        }
-        // button 1: download
-        // firefox doesn't support direct download function.
-        const isFirefox = typeof InstallTrigger !== 'undefined';
-        if (!isFirefox) addButton(panel, "download-set", iconDownload);
-        // button 2: new tab
-        addButton(panel, "newtab-set", iconNewtab);
+    function checkSort() {
+        // sometimes, the "share" button is created slower than this userscript.
+        // this function will sort the button to the original position.
+        document.querySelectorAll("section.ltpMr.Slqrh.section-set").forEach(function(panel) {
+            const count = panel.childElementCount;
+            const penultimate = panel.children[count - 2];
+            if (!penultimate.className.includes("wpO6b")) return;
+            const custom = panel.querySelector(".dCJp8");
+            panel.insertBefore(penultimate, custom);
+        });
     }
 
-    function addButton(panel, className, icon) {
+    function setButton(panel, myClass, icon) {
         // create
         const button = document.createElement("button");
-        button.classList.add("dCJp8", "afkep", className);
+        button.className = `dCJp8 afkep ${myClass}`;
         button.innerHTML = icon;
         button.addEventListener("click", onClick);
         panel.lastElementChild.before(button);
@@ -100,8 +99,8 @@
         else return files[1].src;
     }
 
-    function download(download, link, article) {
-        if (download) {
+    function download(isDownload, link, article) {
+        if (isDownload) {
             fetch(link).then(t => {
                 return t.blob().then(b => {
                     const a = document.createElement("a");
@@ -119,7 +118,7 @@
 
     // function made by Paul Dmytrewycz.
     function getUser(article) {
-        return article.querySelector(".e1e1d a").innerText;
+        return article.querySelector(".e1e1d a").innerText.replace(".", "-");
     }
 
     function getTime(article) {
@@ -138,26 +137,25 @@
         }
     }
 
+    function update() {
+        if (updating) return;
+        updating = true;
+        init(3);
+        setTimeout(() => { updating = false; }, 1000);
+    }
+
     function locationChange() {
-        window.addEventListener('locationchange', init);
-        // situation 1
-        history.pushState = (f => function pushState(){
-            var ret = f.apply(this, arguments);
-            window.dispatchEvent(new Event('pushState'));
-            window.dispatchEvent(new Event('locationchange'));
-            return ret;
-        })(history.pushState);
-        // situation 2
-        history.replaceState = (f => function replaceState(){
-            var ret = f.apply(this, arguments);
-            window.dispatchEvent(new Event('replaceState'));
-            window.dispatchEvent(new Event('locationchange'));
-            return ret;
-        })(history.replaceState);
-        // situation 3
-        window.addEventListener('popstate', () => {
-            window.dispatchEvent(new Event('locationchange'));
+        const observer = new MutationObserver(mutations => {
+            mutations.forEach(() => {
+                if (currentUrl !== document.location.href) {
+                    currentUrl = document.location.href;
+                    init(10);
+                }
+            });
         });
+        const target = document.querySelector("body");
+        const config = { childList: true, subtree: true };
+        observer.observe(target, config);
     }
 
 })();
